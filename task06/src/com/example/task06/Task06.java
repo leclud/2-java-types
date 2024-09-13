b032bf55c51d46b300c6938a9295506fb35b271b
leclud <negativchikxxl@gmail.com>
2024-09-13 06:45:19 +0300

    Task 6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,23 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sum = x + y;
+        int sumlen = 0;
 
-        return 0;
+        while (sum != 0) {
+            sumlen++;
+            sum /= 10;
+        }
+
+        if (sumlen == 0) return 1;
+        return sumlen;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
